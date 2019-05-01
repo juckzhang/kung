7eed5ed85902c8c179ef3d9f173262eeda741f0c
zhangchao <zhangchao_1125@sian.cn>
2019-05-01 22:20:06 +0800

    kung

--- frontend/controllers/TestController.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace frontend\controllers;
-
-class TestController extends BaseController
-{
-    public function actionTest(){
-        var_dump(\Yii::$app->request->headers->get('Access-Token'));
-    }
-}
