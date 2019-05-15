747dd491e620813516ee81373ed26030e6a9ab96
zhangchao <zhangchao_1125@sian.cn>
2019-05-15 22:36:52 +0800

    kung

--- common/models/mysql/MediaDownloadModel.php
+++ common/models/mysql/MediaDownloadModel.php
@@ -11,12 +11,12 @@ class MediaDownloadModel extends ActiveRecord
     public function getMedia()
     {
         return $this->hasOne(MediaModel::className(),['id' => 'source_id'])
-//            ->select([
-//                'id','cate_id','source_type','title',
-//                'sub_title','desc','total_time','level','poster_url',
-//                'play_link','download_link','play_num','collection_num',
-//                'download_num','create_time'
-//            ])
+            ->select([
+                    'id','cate_id','source_type','title',
+                    'title_en','total_time','level','poster_url',
+                    'play_link','play_num','collection_num',
+                    'download_num','create_time'
+            ])
             ->where(['status' => MediaModel::STATUS_ACTIVE])
             ->with('category');
     }
