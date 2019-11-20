74c6808c13245dbc58429804ab102ce5e328efc7
张超 <zhangchao12@staff.sina.com.cn>
2019-11-20 19:15:19 +0800

    kung

--- frontend/config/ad.php
+++ frontend/config/ad.php
@@ -1,11 +1,11 @@
 <?php
 return [
     [
-        'pic' => 'http://ss1.kungchinese.com/',
+        'pic' => 'http://ss1.kungchinese.com/pdf/f9edff123568f5c545c631c67154293b.png',
         'scheme' => '',
     ],
     [
-        'pic' => 'http://ss1.kungchinese.com/',
+        'pic' => 'http://ss1.kungchinese.com/picture/ca53242f3c0b94d346ea0241d210f485.png',
         'scheme' => 'local:fb',
     ],
 ];
