c199f8656b3165a9795ec0680661e5531d68e730
张超 <zhangchao12@staff.sina.com.cn>
2019-11-21 10:46:40 +0800

    kung

--- frontend/config/ad.php
+++ frontend/config/ad.php
@@ -2,7 +2,7 @@
 return [
     [
         'pic' => 'http://ss1.kungchinese.com/picture/1554809b53c8eede1b5c70715c5a32ec.png',
-        'scheme' => '',
+        'scheme' => 'http://106.12.72.118/active/index.html',
     ],
     [
         'pic' => 'http://ss1.kungchinese.com/picture/ca53242f3c0b94d346ea0241d210f485.png',
